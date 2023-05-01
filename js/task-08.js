3f8b350b1e617d9954b47c6bc1fdca20204f563f
Taras-2402 <taraszlepko@gmail.com>
2023-05-01 19:30:02 +0300

    task 8 task 7

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
-
+const form = document.querySelector(".login-form");
+const useremail = document.querySelector('[type="email"]');
+const password = document.querySelector('[type="password"]');
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const elements = {
+    email: useremail.value,
+    pass: password.value
+  }
+  if (useremail.value === '' || password.value === '') {
+    return alert('Все поля должны быть заполнены!')
+  }
+  console.log(elements);
+  form.reset();
+});
